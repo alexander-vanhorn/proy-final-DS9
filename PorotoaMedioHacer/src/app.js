423d04f6680a54a138c78395aa41cafd1be57ba5
Alexander Vanhorn <alexander.vanhorn@utp.ac.pa>
2023-07-16 03:32:30 -0500

    correcion de gitignore

--- PorotoaMedioHacer/src/app.js
+++ PorotoaMedioHacer/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3306;
 const session = require('express-session');
 // Esto nos permite usar archivos estáticos como CSS, imágenes, etc.
 app.use(express.static('public'));
